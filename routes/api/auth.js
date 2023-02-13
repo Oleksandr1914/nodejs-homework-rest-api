94c28f8eb705278bbb9f84554f421d7a6ec92ac9
Oleksandr_Mochakov <sasha.mochakov@ukr.net>
2023-02-13 11:30:53 +0200

    added img

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { authenticate } = require("../../middleware/authenticate");
+const upload = require("../../middleware/upload");
 
 const router = express.Router();
 const user = require("../../controllers/auth");
-const { route } = require("./contacts");
 
 router.post("/register", user.register);
 
@@ -12,5 +12,12 @@ router.post("/login", user.login);
 router.post("/logout", authenticate, user.logout);
 
 router.get("/users/curent", authenticate, user.curent);
+
+router.patch(
+  "/users/avatars",
+  authenticate,
+  upload.single("avatar"),
+  user.updateAvatar
+);
 
 module.exports = router;
